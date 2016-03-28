2982405c865f730f8e257cd3d881ee9abcf8b14a
prlof <prlof@github.com>
2016-03-28 14:42:34 +0100

    badilit djdjd

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-
+from django.apps import AppConfig
 # Register your models here.
 
-class PostsConfig(AppConfig):
+from .models import Post
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["title", "updated", "timestamp"]
+    list_display_links = ["updated"]
+    list_filter = ["updated", "timestamp"]
+    search_fields = ["title"]
+    class Meta:
+        model = Post
     name = 'posts'
+admin.site.register(Post, PostModelAdmin)
