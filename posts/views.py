dce786e174cdf51a91daafbfef0b59bca4975bc2
prlof <aminamri929@gmail.com>
2016-04-05 00:10:54 +0100

    various modifs

--- posts/views.py
+++ posts/views.py
@@ -9,11 +9,23 @@ def post_create(request):
 
 def post_detail(request):
 
-    return HttpResponse("<h1>Detail</h1>")
+    context = {
+            "title": "Detail"
+            }
+    return render(request, "index.html", context)
 
 def post_list(request):
 
-    return HttpResponse("<h1>List</h1>")
+#    if request.user.is_authenticated():
+#        context = {
+#                "title": "BoomFara"
+#                }
+#    else:
+#        context = {
+#                "title": "List"
+#                }
+    return render(request, "index.html", context)
+    #return HttpResponse("<h1>List</h1>")
 
 def post_update(request):
 
--- trydjango19/settings.py
+++ trydjango19/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+#~/projects/trydjango19/src
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'trydjango19.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
