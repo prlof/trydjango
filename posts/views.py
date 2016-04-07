4c85bd3c3cbaa585e7cf4e52b63ed501fb7c9b6e
prlof <aminamri929@gmail.com>
2016-04-07 23:52:45 +0100

    Added a for loop

--- posts/views.py
+++ posts/views.py
@@ -1,20 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.shortcuts import HttpResponse
 
 # Create your views here.
 
+from .models import Post
 def post_create(request):
 
     return HttpResponse("<h1>Create</h1>")
 
 def post_detail(request):
 
+    #instance = Post.objects.get(id=66)
+    instance = get_object_or_404(Post, title = "Hi")
     context = {
             "title": "Detail"
             }
     return render(request, "index.html", context)
 
 def post_list(request):
+
+    queryset = Post.objects.all()
+    context = {
+            "object_list" : queryset,
+            "title" : "List"
+            }
 
 #    if request.user.is_authenticated():
 #        context = {
